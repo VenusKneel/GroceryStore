a7818b265ecfc07950e89b8882c72213089db3b3
Lado <Tsivtsivadzelado@yahoo.com>
2021-07-08 05:12:38 -0700

    added search storyboard
    added search controller swift file

---
+++ GroceryStore/Presentation/Scenes/Search/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GroceryStore
+//
+//  Created by Ladolado3911 on 08.07.21.
+//
+
+import UIKit
+
+class SearchViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
