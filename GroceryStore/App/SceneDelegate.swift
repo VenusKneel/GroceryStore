50cf32c3ad01f050755d979d59b021d758296ff5
Beqa <beqa.tabunidze@icloud.com>
2021-07-08 17:26:36 +0400

    - Fixed sceneDelegate to adapt coordinator pattern

--- GroceryStore/App/SceneDelegate.swift
+++ GroceryStore/App/SceneDelegate.swift
@@ -10,13 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var coordinator: CoordinatorProtocol?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        configureRootVC(with: scene)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -45,11 +47,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-
-        // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
+    func configureRootVC(with scene: UIWindowScene) {
+            window = UIWindow(windowScene: scene)
+            coordinator = AppCoordinator(window, navigationController: UINavigationController())
+            coordinator?.start()
+    }
 
 }
 
