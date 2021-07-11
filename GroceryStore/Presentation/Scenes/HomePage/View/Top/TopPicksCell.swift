47ac941d8ca2ab24e612cbf4025f131f3915fd0c
BakhvaJakeli <lordbaxva@gmail.com>
2021-07-11 16:22:35 +0400

    Final Home Page

--- GroceryStore/Presentation/Scenes/HomePage/View/Top/TopPicksCell.swift
+++ GroceryStore/Presentation/Scenes/HomePage/View/Top/TopPicksCell.swift
@@ -19,16 +19,4 @@ class TopPicksCell: UITableViewCell {
     @IBAction func onMenu(_ sender: Any) {
     }
     
-    //MARK: - ბეზიერით მინდოდა გაკეთება და ეს კოდი აჩენდა ყველაფერს მაგრამ მერე ლეიბლები და ბათონები აღარ ჩანდა არადა 3დ სიმულატორზე რომ ვნახულობდი ვფიქრობ უნდა გამოჩენილიყო
-    
-//    private func addCenter() {
-//        let circlePath = UIBezierPath(arcCenter: CGPoint(x: contentView.bounds.width / 2 , y: 20), radius: CGFloat(243), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
-//        let shapeLayer = CAShapeLayer()
-//        shapeLayer.path = circlePath.cgPath
-//        shapeLayer.fillColor = UIColor(named: "Green")?.cgColor
-//        shapeLayer.strokeColor =  UIColor(named: "Green")?.cgColor
-//        shapeLayer.lineWidth = 3.0
-//        topView.layer.addSublayer(shapeLayer)
-//    }
-    
 }
